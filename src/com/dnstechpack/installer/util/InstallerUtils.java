5978ccf9fc2c36101fdbad98dac4d7c2d51d12ac
ShadowChild <shadychild12@gmail.com>
2013-08-14 10:58:27 +0100

    Use apache commons
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

--- src/com/dnstechpack/installer/util/InstallerUtils.java
+++ src/com/dnstechpack/installer/util/InstallerUtils.java
@@ -1,11 +1,9 @@
 package com.dnstechpack.installer.util;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class InstallerUtils {
 
@@ -28,40 +26,42 @@ public class InstallerUtils {
 	
 	public static void moveFolder(File src, File dest) throws IOException {
 
-		if (src.isDirectory()) {
+        FileUtils.moveFile(src, dest);
 
-			if (!dest.exists()) {
-				dest.mkdir();
-				System.out.println("Directory copied from " + src + "  to " + dest);
-			}
-
-			String files[] = src.list();
-
-			for (String file : files) {
-				
-				File srcFile = new File(src, file);
-				File destFile = new File(dest, file);
-				
-				moveFolder(srcFile, destFile);
-			}
-
-		} else {
-
-			InputStream in = new FileInputStream(src);
-			OutputStream out = new FileOutputStream(dest);
-
-			byte[] buffer = new byte[1024];
-
-			int length;
-
-			while ((length = in.read(buffer)) > 0) {
-				out.write(buffer, 0, length);
-			}
-
-			in.close();
-			out.close();
-			System.out.println("File copied from " + src + " to " + dest);
-		}
+//		if (src.isDirectory()) {
+//
+//			if (!dest.exists()) {
+//				dest.mkdir();
+//				System.out.println("Directory copied from " + src + "  to " + dest);
+//			}
+//
+//			String files[] = src.list();
+//
+//			for (String file : files) {
+//
+//				File srcFile = new File(src, file);
+//				File destFile = new File(dest, file);
+//
+//				moveFolder(srcFile, destFile);
+//			}
+//
+//		} else {
+//
+//			InputStream in = new FileInputStream(src);
+//			OutputStream out = new FileOutputStream(dest);
+//
+//			byte[] buffer = new byte[1024];
+//
+//			int length;
+//
+//			while ((length = in.read(buffer)) > 0) {
+//				out.write(buffer, 0, length);
+//			}
+//
+//			in.close();
+//			out.close();
+//			System.out.println("File copied from " + src + " to " + dest);
+//		}
 	}
 
     public static String getMCVersion() {
