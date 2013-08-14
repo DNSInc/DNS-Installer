140698883695e5b1b6cabbd02c54e2e04da51c30
Darkhax <darklime@live.ca>
2013-08-13 23:31:00 -0600

    Added something to move files around. This is meant for moving the mods in the installer into the install directory.

--- src/com/dnstechpack/installer/util/InstallerUtils.java
+++ src/com/dnstechpack/installer/util/InstallerUtils.java
@@ -1,11 +1,17 @@
 package com.dnstechpack.installer.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class InstallerUtils {
 
 	public static File dnsDefault = new File(System.getProperty("user.home"), "AppData/Roaming/.dns");
     public static File mcDefault = new File(System.getProperty("user.home"), "AppData/Roaming/.minecraft");
+    public static File mods = new File("/mods");
 	
 	static {
 		
@@ -18,6 +24,44 @@ public class InstallerUtils {
 
             mcDefault.mkdirs();
         }
+	}
+	
+	public static void moveFolder(File src, File dest) throws IOException {
+
+		if (src.isDirectory()) {
+
+			if (!dest.exists()) {
+				dest.mkdir();
+				System.out.println("Directory copied from " + src + "  to " + dest);
+			}
+
+			String files[] = src.list();
+
+			for (String file : files) {
+				
+				File srcFile = new File(src, file);
+				File destFile = new File(dest, file);
+				
+				moveFolder(srcFile, destFile);
+			}
+
+		} else {
+
+			InputStream in = new FileInputStream(src);
+			OutputStream out = new FileOutputStream(dest);
+
+			byte[] buffer = new byte[1024];
+
+			int length;
+
+			while ((length = in.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
+			}
+
+			in.close();
+			out.close();
+			System.out.println("File copied from " + src + " to " + dest);
+		}
 	}
 
     public static String getMCVersion() {
