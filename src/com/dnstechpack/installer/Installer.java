33a98a5ed47cda24108850e653509e05dbbafa56
ShadowChild <shadychild12@gmail.com>
2013-08-18 21:30:09 +0100

    Y NOBODY LIEK IT?!?!
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

--- src/com/dnstechpack/installer/Installer.java
+++ src/com/dnstechpack/installer/Installer.java
@@ -4,20 +4,11 @@ import com.dnstechpack.installer.gui.InstallerFrame;
 import com.dnstechpack.installer.util.InstallerUtils;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 
 public class Installer {
 
     public static void main(String... args) {
-
-        try {
-
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch(Throwable e) {
-
-            e.printStackTrace();
-        }
 
         SwingUtilities.invokeLater(new Runnable() {
 
